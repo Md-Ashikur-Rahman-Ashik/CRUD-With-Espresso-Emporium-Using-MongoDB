ea4cfc5bb16d762703ae471b28e4b6446eac5921
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-04-22 10:17:49 +0600

    Server Side  Database Hidden
    User And Password Hidden with Dotenv

--- coffee-store-client/src/components/AddCoffee.jsx
+++ coffee-store-client/src/components/AddCoffee.jsx
@@ -1,9 +1,47 @@
 const AddCoffee = () => {
-    return (
-        <div>
-            <h2>Add A Coffee</h2>
+  return (
+    <div>
+      <h2>Add A Coffee</h2>
+      <form>
+        <div className="flex">
+          <div className="w-1/5">
+            <label
+              htmlFor="url"
+              className="block text-sm font-medium text-black"
+            >
+              Coffee Name
+            </label>
+            <div className="flex">
+              <input
+                type="text"
+                name="url"
+                id="url"
+                placeholder="Coffee Name"
+                className="flex py-2 flex-1 border sm:text-sm rounded-r-md focus:ring-inset border-gray-300 px-2 text-black bg-white focus:ring-violet-400"
+              />
+            </div>
+          </div>
+          <div className="w-1/5">
+            <label
+              htmlFor="url"
+              className="block text-sm font-medium text-black"
+            >
+              Available Quantity
+            </label>
+            <div className="flex">
+              <input
+                type="text"
+                name="url"
+                id="url"
+                placeholder="Available Quantity"
+                className="flex py-2 flex-1 border sm:text-sm rounded-r-md focus:ring-inset border-gray-300 px-2 text-black bg-white focus:ring-violet-400"
+              />
+            </div>
+          </div>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default AddCoffee;
--- coffee-store-server/index.js
+++ coffee-store-server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,14 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// coffeeMaster
-// K9n7ObvC3pXiyGC8
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tx9lkv1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// const uri =
-//   "mongodb+srv://<username>:<password>@cluster0.tx9lkv1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-const uri =
-  "mongodb+srv://coffeeMaster:K9n7ObvC3pXiyGC8@cluster0.tx9lkv1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -37,7 +33,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
